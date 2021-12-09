6c6e95f6d1791bd90c548e8ece28f9deb4ab28f3
Noa <noavivi79@gmail.com>
2021-12-09 16:53:54 +0200

    create MessageObject :)

---
+++ src/main/java/com/dev/objects/MessageObject.java
@@ -0,0 +1,67 @@
+package com.dev.objects;
+
+public class MessageObject {
+    private int messageId;
+    private int senderId;
+    private int receiverId;
+    private String title;
+    private String message;
+    private String sendDate;
+    private boolean read;
+
+    public int getMessageId() {
+        return messageId;
+    }
+
+    public void setMessageId(int messageId) {
+        this.messageId = messageId;
+    }
+
+    public int getSenderId() {
+        return senderId;
+    }
+
+    public void setSenderId(int senderId) {
+        this.senderId = senderId;
+    }
+
+    public int getReceiverId() {
+        return receiverId;
+    }
+
+    public void setReceiverId(int receiverId) {
+        this.receiverId = receiverId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getSendDate() {
+        return sendDate;
+    }
+
+    public void setSendDate(String sendDate) {
+        this.sendDate = sendDate;
+    }
+
+    public boolean isRead() {
+        return read;
+    }
+
+    public void setRead(boolean read) {
+        this.read = read;
+    }
+}
